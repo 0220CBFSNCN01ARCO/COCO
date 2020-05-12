439c1f2421f1dcbc1af287dec27f28b8e4301e67
Franco-Casuscelli <franco.casuscelli5@gmail.com>
2020-05-12 20:53:01 -0300

    agregando /users

--- site/routes/users.js
+++ site/routes/users.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('login');
+});
+/* GET users listing. */
+router.get('/login', function(req, res, next) {
+  res.render('login');
 });
 
 module.exports = router;
