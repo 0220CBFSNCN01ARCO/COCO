f760a50a912433239f95350dfd610243ea348e34
Franco-Casuscelli <franco.casuscelli5@gmail.com>
2020-05-14 10:26:56 -0300

    agregando 404

--- site/app.js
+++ site/app.js
@@ -34,7 +34,7 @@ app.use('/register', registerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  res.render("404");
 });
 
 // error handler
