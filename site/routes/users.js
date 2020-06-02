c596e946432e49e15b607a4e24ac8b87e4012867
Franco-Casuscelli <franco.casuscelli5@gmail.com>
2020-06-02 12:39:35 -0300

    terminando register

--- site/routes/users.js
+++ site/routes/users.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var router = express.Router();
 const usersController = require('../controllers/usersController.js')
+const multer = require('multer');
+const path = require("path")
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'public/images/users');
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now()  + path.extname(file.originalname));
+    }
+  });
 
 /* GET users listing. */
 router.get('/', usersController.login);
 
 router.get('/register', usersController.register);
+
+//router.post('/register',upload.any() , usersController.Sendregister);
 
 router.get('/bag', usersController.bag);
 
