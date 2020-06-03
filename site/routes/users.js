92feceb3180812c827011c91d6e9e7faf5c4f3a3
Franco-Casuscelli <franco.casuscelli5@gmail.com>
2020-06-03 11:16:35 -0300

    Validando Email existente

--- site/routes/users.js
+++ site/routes/users.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 const usersController = require('../controllers/usersController.js')
 const multer = require('multer');
@@ -25,6 +26,26 @@ router.post('/register', upload.any(), [
     check("Firs_name").isLength(),
     check("Last_name").isLength(),
     check("Email").isEmail(),
+    body("Email").custom(function(value){
+        
+        let UsersJSON = path.join(__dirname, '../data/users.json');
+        let usersList = JSON.parse(fs.readFileSync(UsersJSON, 'utf-8'));
+
+            let users;
+            if (UsersJSON = "") {
+                users = []
+            }else{
+                users = usersList;
+            }
+        
+        for(let i = 0; i < users.length; i++){
+            if (users[i].email == value){
+                return false
+            }
+        }
+        return true
+
+    }).withMessage("The email is already in use"),
     check("Password").isLength({min: 6}).withMessage("The password must contain 6 characters"),
     check("password repeat").isLength()
 ], usersController.create);
